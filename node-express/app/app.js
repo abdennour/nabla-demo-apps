ab71490fdd9fd218417889569bac36b7302df1dc
abdennour <mail@abdennoor.com>
2018-07-21 18:27:06 +0300

    add line

--- node-express/app/app.js
+++ node-express/app/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Nabla!'))
+app.get('/', (req, res) => res.send('Nabla!\n'))
 
 app.listen(8080, () => console.log('Listening on port 8080'))
